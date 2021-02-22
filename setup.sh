325ccc5bb04fb5e304e5cdc460b15ed9e5691d17
QuentinN42 <quentin.lieumont@gmail.com>
2021-02-22 19:42:28 +0100

    setup done

--- setup.sh
+++ setup.sh
@@ -29,3 +29,17 @@ touch data/TodoDiscord.txt
     tar -xvf geckodriver-v0.29.0-linux64.tar.gz
     rm geckodriver-v0.29.0-linux64.tar.gz
 )
+
+
+
+
+cat << EOF
+All done,
+You must add a python file named secrets.py in the display_games folder with :
+BOT_TOKEN -> the token for the discord bot
+channel   -> the channel id where the bot speak
+role      -> the role id to ping
+
+And add this lines to your crontab :
+EOF
+envsubst < crontab
