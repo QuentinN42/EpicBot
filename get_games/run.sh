a424f3b316e0295d027cffc4835b3def0592df3c
QuentinN42 <quentin.lieumont@gmail.com>
2021-02-22 19:16:45 +0100

    run get update

--- get_games/run.sh
+++ get_games/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-source venv/bin/activate
-
-python3 get.py >> ./logs 2>&1
-
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && \
+source venv/bin/activate && \
+python3 get.py >> ./logs 2>&1 && \
 deactivate
